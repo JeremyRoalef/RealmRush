a79250cf6fda60e982d2eba03506c8ef8ed8f7b8
JeremyRoalef <roalef944@morrisville.edu>
2024-10-28 13:23:08 -0400

    5.7 Introducing Coroutines

--- RealmRush/Assets/Enemy/EnemyMover.cs
+++ RealmRush/Assets/Enemy/EnemyMover.cs
@@ -6,25 +6,25 @@ public class EnemyMover : MonoBehaviour
 {
     //pass in a bunch of waypoints for the enemy to move to and then loop through those waypoints to move the enemy to the location
     [SerializeField] List<Waypoint> path = new List<Waypoint>(); //Use a list b/c we will mess with the size of the elements
+    [SerializeField] float fltMoveWaitTime = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
+        //Debug.Log("start");
+        StartCoroutine(PrintWaypointName());
+        //Debug.Log("finish");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PrintWaypointName()
     {
-        
-    }
-
-    void PrintWaypointName()
-    {
+        //Delay each waypoint by 1 second (or variable time)
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(fltMoveWaitTime);
+            //Debug.Log(waypoint);
         }
     }
 }
