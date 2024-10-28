a3a96bd7771577b47193b11de64708dd0864d721
JeremyRoalef <roalef944@morrisville.edu>
2024-10-28 17:26:31 -0400

    5.18 Target Closest Enemy

--- RealmRush/Assets/Tower/TargetLocator.cs
+++ RealmRush/Assets/Tower/TargetLocator.cs
@@ -1,24 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
     [SerializeField] Transform target;
-
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform; //only targets the first enemy in hierarchy
-    }
+    [SerializeField] ParticleSystem projectileParticles;
+    [SerializeField] float fltTowerRange = 15f;
 
     void Update()
     {
+        TargetEnemy();
         AimWeapon();
+    }
+    
+    void TargetEnemy()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float fltMaxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < fltMaxDistance )
+            {
+                closestTarget = enemy.transform;
+                fltMaxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
-        weapon.transform.LookAt(target);
+        if (target == null)
+        {
+            Attack(false);
+            return;
+        }
+
+
+        float fltTargetDistance = Vector3.Distance(transform.position, target.position);
+        if (fltTargetDistance < fltTowerRange)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emmissionModule = projectileParticles.emission;
+        emmissionModule.enabled = isActive;
+        if (target != null)
+        {
+            weapon.transform.LookAt(target);
+        }
     }
 }
